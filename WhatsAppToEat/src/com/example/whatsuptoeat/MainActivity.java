e08f72720ac6d7cb0a20e70eded7edab266105eb
Sebastian <sebastian.bantz>
2013-05-27 17:33:54 +0200

    Added the Source code + newer UML

---
+++ WhatsAppToEat/src/com/example/whatsuptoeat/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.whatsuptoeat;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	public void gotoVerwalten(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, VerwaltenActivity.class);
+		startActivity(intent);
+	}
+	
+	public void gotoSuche(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, SucheActivity.class);
+		startActivity(intent);
+	}
+	
+	public void gotoEinkaufskorb(View view) {
+	    // Do something in response to button
+		Intent intent = new Intent(this, EinkaufskorbActivity.class);
+		startActivity(intent);
+	}
+
+}
